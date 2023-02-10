5b954f9cb6143ba056738d13df3416c5a96d6051
springfil <st.kenzhaev@gmail.com>
2023-02-10 23:58:25 +0300

    2 глава учебника , до 2.8

---
+++ learnJS P1_P2/alert_prompt_confirm/p 2_6.js
@@ -0,0 +1,14 @@
+// alert
+alert("Hello");
+
+//Понятие модальное означает,
+//что пользователь не может взаимодействовать с интерфейсом остальной части страницы
+
+//prompt
+/*
+result = prompt(title, [default]);// при cansel/esc-> возвращает null 
+*/
+
+//confirm  OK/cancel -> true/false
+
+
---
+++ learnJS P1_P2/alert_prompt_confirm/t 2_6.js
@@ -0,0 +1,3 @@
+"use strict";
+let userName = prompt('who are u?','');
+alert(userName)

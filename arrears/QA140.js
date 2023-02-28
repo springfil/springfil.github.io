f8ddeedf1331998385178d5c4e166de007e9fa1d
springfil <st.kenzhaev@gmail.com>
2023-02-28 22:16:17 +0300

    долги и теория

---
+++ arrears/QA140.js
@@ -0,0 +1,20 @@
+// к чему принадлежит NaN
+
+typeof NaN // number 
+
+// проверка на NaN
+// Методы isNaN  и  x !== x
+
+
+// if/else 
+let test = true;
+let test1 = true;
+if (test && test1){
+ console.log('test && test1');
+} else if (test) {
+ console.log('test');
+} else if (test1) {
+ console.log('test 1')
+} else {
+    console.log("****")
+} 

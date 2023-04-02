490fb9a955ea8cb427984d9e089e19ff94cadbd8
springfil <st.kenzhaev@gmail.com>
2023-04-02 04:07:45 +0300

    qa

---
+++ learnJS_p5_datatypes/map-set/t6.js
@@ -0,0 +1,16 @@
+// Допустим, у нас есть массив arr.
+// Создайте функцию unique(arr), которая вернёт массив уникальных, не повторяющихся значений массива arr.
+// Например:
+// function unique(arr) {
+//   /* ваш код */
+return Array.from(new Set(arr));
+// }
+
+// let values = ["Hare", "Krishna", "Hare", "Krishna",
+//   "Krishna", "Krishna", "Hare", "Hare", ":-O"
+// ];
+
+// alert( unique(values) ); // Hare,Krishna,:-O
+
+
+// РЕШУ , не хочу подгядывать) 

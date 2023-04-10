64d6c0258229490a8a1d93aa9f284b8fc1cfe3be
springfil <st.kenzhaev@gmail.com>
2023-04-10 23:06:41 +0300

    рекурсивный способ

--- learnJS_p6_advanced-functions/QA_06_04.js
+++ learnJS_p6_advanced-functions/QA_06_04.js
@@ -14,3 +14,27 @@ arr[Symbol.iterator] = function () {
 for (const item of arr) {
   console.log(item);
 }
+// index-- 5.4.3.2
+
+const iterator = arr[Symbol.iterator]();
+
+console.log(iterator.next().value); // 5
+console.log(iterator.next().value); // 4
+console.log(iterator.next().value); // 3
+console.log(iterator.next().value); // 2
+console.log(iterator.next().value); // 1
+console.log(iterator.next().value); // undefined done - true
+
+// или нафиг надо, но пусть будет
+{
+function iterate(iterator) {
+  const result = iterator.next();
+
+  if (!result.done) {
+    console.log(result.value);
+    iterate(iterator);
+  }
+}
+
+iterate(arr[Symbol.iterator]());
+}

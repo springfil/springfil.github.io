2457ad52c404a7fdf6f1a0e6788b2e40647ac0c6
springfil <st.kenzhaev@gmail.com>
2023-04-11 23:20:39 +0300

    :bug:

--- learnJS_p6_advanced-functions/QA_artem.js
+++ learnJS_p6_advanced-functions/QA_artem.js
@@ -3,32 +3,34 @@
 // Применить Symbol.iterator чтобы деструкторизировать без ошибок.
 
 const obj = {
-    name: "Tim",
-    surname: "Kenzhaev",
-    cash: 150000,
-  };
-  
-  obj[Symbol.iterator] = function () {
-    const properties = Object.keys(this);
-    let index = 0;
-  
-    return {
-      next: () => {
-        if (index < properties.length) {
-          const property = properties[index];
-          index++;
-          return { value: this[property], done: false };
-        } else {
-          return { done: true };
-        }
-      },
-    };
-  };
-  
-  const [name, surname, cash] = obj;
-  console.log(name, surname, cash);
+  name: "Tim",
+  surname: "Kenzhaev",
+  cash: 150000,
+};
 
-// Деструктрурировать массив как объект 
-const arr7 = [2, 1, undefined, 5, null];
-const obj7 = Object.fromEntries(arr7.entries());
-console.log(obj7);
+obj[Symbol.iterator] = function () {
+  const properties = Object.keys(this);
+  let index = 0;
+
+  return {
+    next: () => {
+      if (index < properties.length) {
+        const property = properties[index];
+        index++;
+        return { value: this[property], done: false };
+      } else {
+        return { done: true };
+      }
+    },
+  };
+};
+
+const [name, surname, cash] = obj;
+console.log(name, surname, cash);
+
+// Деструктрурировать массив как объект
+{
+  const arr = [2, 1, undefined, 5, null];
+  const obj = Object.fromEntries(arr.entries());
+  console.log(obj);
+}

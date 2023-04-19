3c596485e4f6c92ce36fb5be078c0e4882562b27
springfil <st.kenzhaev@gmail.com>
2023-04-19 18:22:37 +0300

    не хватает хранения состояния

--- learnJS_p6_advanced-functions/8_settimeout-setinterval/mycode.js
+++ learnJS_p6_advanced-functions/8_settimeout-setinterval/mycode.js
@@ -40,31 +40,30 @@ let timerShow = document.getElementById("timer");
 let timerId;
 
 buttonRun.addEventListener("click", function () {
-    if (!timerId) {
-        let timeMinut = parseInt(timerInput.value) * 60;
+  if (!timerId) {
+    let timeMinut = parseInt(timerInput.value) * 60;
 
-        timerId = setInterval(function () {
-            let seconds = timeMinut % 60;
-            let minutes = (timeMinut / 60) % 60;
-            let hour = (timeMinut / 60 / 60) % 60;
+    timerId = setInterval(function () {
+      let seconds = timeMinut % 60;
+      let minutes = (timeMinut / 60) % 60;
+      let hour = (timeMinut / 60 / 60) % 60;
 
-            if (timeMinut <= 0) {
-                clearInterval(timerId);
-                timerId = null;
-                alert("Отсчет остановлен");
-            } else {
-                let strTimer = `${Math.trunc(hour)}:${Math.trunc(minutes)}:${seconds}`;
-                timerShow.innerHTML = strTimer;
-            }
+      if (timeMinut <= 0) {
+        clearInterval(timerId);
+        timerId = null;
+        alert("Отсчет остановлен");
+      } else {
+        let strTimer = `${Math.trunc(hour)}:${Math.trunc(minutes)}:${seconds}`;
+        timerShow.innerHTML = strTimer;
+      }
 
-            --timeMinut;
-
-        }, 1000);
-    }
+      --timeMinut;
+    }, 1000);
+  }
 });
 
 buttonStop.addEventListener("click", () => {
-    clearInterval(timerId);
-    timerId = null;
-    alert("Отсчет остановлен");
+  clearInterval(timerId);
+  timerId = null;
+  alert("Отсчет остановлен");
 });

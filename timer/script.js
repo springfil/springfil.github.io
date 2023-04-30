c1a7c275cea29aa4d9a13bbdf90c4f4d5605f767
springfil <st.kenzhaev@gmail.com>
2023-04-30 03:03:34 +0300

    qa

--- timer/script.js
+++ timer/script.js
@@ -1,71 +1,84 @@
-const hoursInput = document.getElementById("hours");
-const minutesInput = document.getElementById("minutes");
-const secondsInput = document.getElementById("seconds");
-const startButton = document.getElementById("start");
-const stopButton = document.getElementById("stop");
-const resetButton = document.getElementById("reset");
-const countdownDiv = document.getElementById("countdown");
+const HOURS_INPUT = document.getElementById("hours");
+const MINUTES_INPUT = document.getElementById("minutes");
+const SECONDS_INPUT = document.getElementById("seconds");
+const START_BUTTON = document.getElementById("start");
+const STOP_BUTTON = document.getElementById("stop");
+const RESET_BUTTON = document.getElementById("reset");
+const COUNT_DOWN_DIV = document.getElementById("countdown");
 
 let countdown;
-let remainingMilliseconds; // переменная для сохранения оставшегося времени на таймере
+let remainingMilliseconds; 
 
 function antiPaste(e) {
-  const clipboardData = e.clipboardData.getData("text/plain");// получили текст скопированный в буфер
-  const regex = /^-?\d*\.?\d+$/g; // .number -number
+  const clipboardData = e.clipboardData.getData("text/plain"); 
+  const regex = /^-?\d*\.?\d+$/g; 
 
-  if (regex.test(clipboardData)) { 
+  if (regex.test(clipboardData)) {
     e.preventDefault();
     alert("Нельзя вставить такое число");
   }
 }
 
-hoursInput.addEventListener("paste", antiPaste);
-minutesInput.addEventListener("paste", antiPaste);
-secondsInput.addEventListener("paste", antiPaste);
+function handleInput(event) {
+  if (event.keyCode === 189 || (this.value === "" && event.keyCode === 109)) {
+    event.preventDefault();
+  }
+}
+
+[HOURS_INPUT, MINUTES_INPUT, SECONDS_INPUT].forEach((input) => {
+  input.addEventListener("keydown", handleInput);
+});
+
+[HOURS_INPUT, MINUTES_INPUT, SECONDS_INPUT].forEach((input) => {
+  input.addEventListener("paste", antiPaste);
+});
+
+function validateTime(...times) {
+  if (times.every((time) => Number.isNaN(time) || time < 0)) {
+    alert("Введите корректные значения для времени!");
+    return false;
+  }
+  return true;
+}
 
 function startCountdown() {
-  const hours = Number(hoursInput.value);
-  const minutes = Number(minutesInput.value);
-  const seconds = Number(secondsInput.value);
+  clearInterval(countdown);
 
-  if (
-    isNaN(hours) ||
-    isNaN(minutes) ||
-    isNaN(seconds) ||
-    hours < 0 ||
-    minutes < 0 ||
-    seconds < 0
-  ) {
-    alert("Введите корректные значения для времени!");
+  const [hours, minutes, seconds] = [
+    HOURS_INPUT,
+    MINUTES_INPUT,
+    SECONDS_INPUT,
+  ].map((input) => input.value);
+
+  if (hours == 0 && minutes == 0 && seconds == 0) {
+    alert("Введите время!");
+    return;
+  }
+
+  if (!validateTime(hours, minutes, seconds)) {
     return;
   }
 
   let totalMilliseconds;
   if (remainingMilliseconds) {
-    // если время было остановлено, используем сохраненное время
     totalMilliseconds = remainingMilliseconds;
   } else {
     totalMilliseconds = (hours * 60 * 60 + minutes * 60 + seconds) * 1000;
   }
 
-  countdownDiv.innerHTML = formatCountdown(totalMilliseconds);
+  COUNT_DOWN_DIV.innerHTML = formatCountdown(totalMilliseconds);
 
-  countdown = setInterval(() => {
-    totalMilliseconds--;
+ countdown = setInterval(() => {
+    totalMilliseconds -= 15;
 
-    if (totalMilliseconds < 0) {
+    if (totalMilliseconds <= 0) {
       clearInterval(countdown);
-      countdownDiv.innerHTML = "0:00:00.000";
+      COUNT_DOWN_DIV.innerHTML = "Время вышло!";
       return;
     }
 
-    countdownDiv.innerHTML = formatCountdown(totalMilliseconds);
-
-    if (totalMilliseconds == 0) {
-      clearInterval(countdown);
-      countdownDiv.innerHTML = "Время вышло!";
-    }
-  }, 1);
+    COUNT_DOWN_DIV.innerHTML = formatCountdown(totalMilliseconds);
+  }, 15);
 }
 
 function formatCountdown(totalMilliseconds) {
@@ -83,19 +96,33 @@ function formatCountdown(totalMilliseconds) {
     .padStart(3, "0")}`;
 }
 
-function stopCountdown() {
-  clearInterval(countdown);
-}
-
 function resetCountdown() {
   clearInterval(countdown);
-  countdownDiv.innerHTML = "";
-  hoursInput.value = 0;
-  minutesInput.value = 0;
-  secondsInput.value = 0;
-  remainingMilliseconds = null; // обнуляем сохраненное время при сбросе таймера
+  COUNT_DOWN_DIV.innerHTML = "";
+  [HOURS_INPUT, MINUTES_INPUT, SECONDS_INPUT].forEach(
+    (input) => (input.value = 0)
+  );
+  remainingMilliseconds = null; 
 }
 
-startButton.addEventListener("click", startCountdown);
-stopButton.addEventListener("click", stopCountdown);
-resetButton.addEventListener("click", resetCountdown);
+START_BUTTON.addEventListener("click", startCountdown);
+STOP_BUTTON.addEventListener("click", () => clearInterval(countdown));
+RESET_BUTTON.addEventListener("click", resetCountdown);
+
+{
+  /*
+  upper pitoncase +
+  numberisnan - вынести проверку в переменную и извалидинпут +
+  сдеелать синглреспосибилити - функции отвечали +-
+  onkeydown в скрипт +
+  
+  формат коунтдеструктурировал 
+  подумать как сделать без реманинг /вызвать функцию на 46-й строке
+  оповестить ,что нельзя нажать старт 
+  дизейбл кнопок 
+  
+  На следующий синк 
+  //свой помодоро таймер 
+  // есть помидорка свгшка 
+*/
+}

a8ca1ad2d25797131c481485cbaa0c6fd4ce8f34
springfil <st.kenzhaev@gmail.com>
2023-04-30 04:04:34 +0300

    )

--- learnJS_p7_object_properties/2_property-accessors/mycode.js
+++ learnJS_p7_object_properties/2_property-accessors/mycode.js
@@ -47,3 +47,40 @@ aboba.name = "Доба";
 console.log(aboba.name);
 aboba.age = "zopa"; // Error('Возраст должен быть числом');
 console.log(aboba.age);
+
+
+
+let obj = {};
+
+Object.defineProperty(obj, "maxim", {
+  get() {
+    return obj._maxim.toUpperCase();
+  },
+  set(value) {
+    obj._maxim = value.toUpperCase();
+  },
+  enumerable: true,
+});
+console.log(obj);
+obj.maxim = "zopa";
+console.log(obj.maxim);
+console.log(obj);//{maxim: "ZOPA", _maxim: "ZOPA"}
+
+
+
+const obj1 = {
+  maximValue: "",
+
+  get maxim() {
+    return this.maximValue.toUpperCase();
+  },
+
+  set maxim(value) {
+    this.maximValue = value;
+  },
+};
+
+obj1.maxim = "zopa";
+
+console.log(obj.maxim); // "ZOPA"
+console.log(obj1);//{maximValue: "zopa", maxim: "ZOPA"}

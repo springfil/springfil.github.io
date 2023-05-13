8e5d80aced17531b0b3540b3dbe3f931c74bdcf4
springfil <st.kenzhaev@gmail.com>
2023-05-13 21:09:36 +0300

    базовый синтаксис классов

--- learnJS_p8_prototype/p_4_prototype-methods/t4.js
+++ learnJS_p8_prototype/p_4_prototype-methods/t4.js
@@ -1,47 +1,44 @@
-
-{// TASK1
-// let dictionary = Object.create(null);
-
-// // ваш код, который добавляет метод dictionary.toString
-
-// // добавляем немного данных
-// dictionary.apple = "Apple";
-// dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
-
-// // только apple и __proto__ выведены в цикле
-// for(let key in dictionary) {
-//   alert(key); // "apple", затем "__proto__"
-// }
-
-// // ваш метод toString в действии
-// alert(dictionary); // "apple,__proto__"
+{
+  // TASK1
+  // let dictionary = Object.create(null);
+  // // ваш код, который добавляет метод dictionary.toString
+  // // добавляем немного данных
+  // dictionary.apple = "Apple";
+  // dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
+  // // только apple и __proto__ выведены в цикле
+  // for(let key in dictionary) {
+  //   alert(key); // "apple", затем "__proto__"
+  // }
+  // // ваш метод toString в действии
+  // alert(dictionary); // "apple,__proto__"
 }
 
-let dictionary = Object.create(null,{
-    toString: {
-        value() {
-            return Object.keys(this).join();
-        }
-    }
+let dictionary = Object.create(null, {
+  toString: {
+    value() {
+      return Object.keys(this).join();
+    },
+  },
 });
 
+{
+  //task2
 
+  function Rabbit(name) {
+    this.name = name;
+  }
 
-{//task2
+  Rabbit.prototype.sayHi = function () {
+    alert(this.name);
+  };
 
-    function Rabbit(name) {
-      this.name = name;
-    }
+  let rabbit = new Rabbit("Rabbit");
+  //Все эти вызовы делают одно и тоже или нет?
 
-    Rabbit.prototype.sayHi = function() {
-      alert(this.name);
-    };
-    
-    let rabbit = new Rabbit("Rabbit");
-    //Все эти вызовы делают одно и тоже или нет?
-    
-    rabbit.sayHi(); // Rabbit
-    Rabbit.prototype.sayHi(); 
-    Object.getPrototypeOf(rabbit).sayHi();
-    rabbit.__proto__.sayHi();
+  rabbit.sayHi(); // Rabbit
+  Rabbit.prototype.sayHi();
+  Object.getPrototypeOf(rabbit).sayHi();
+  rabbit.__proto__.sayHi();
 }
+
+
---
+++ learnJS_p9_classes/p1_class/p1.js
@@ -0,0 +1,147 @@
+{ //базовый синтаксис класса
+    {
+        class MyClass {
+            // методы класса
+            constructor() { ... }
+            method1() { ... }
+            method2() { ... }
+            method3() { ... }
+            ...
+          }
+    }
+    // методы не разделяются запятой
+    {
+        class Abobus{
+
+            constructor(name){
+                this.name = name;
+            }
+
+            say(){
+                console.log(this.name + " -абобус")
+            }
+        }
+
+        let aboba = new Abobus('Beluga');
+        aboba.say(); //Beluga -абобус
+
+        console.log(typeof Abobus) //function
+        console.log(Abobus === Abobus.prototype.constructor.prototype) // true
+        console.log(Abobus.prototype.say) // ƒ say()
+        console.log(Object.getOwnPropertyNames(Abobus.prototype)) //["constructor", "say"]
+    }
+
+}
+
+
+
+{//отличие классов
+
+    // - Во-первых, функция, созданная с помощью class, 
+    // помечена специальным внутренним свойством [[IsClassConstructor]]: true.
+
+    // - В отличие от обычных функций, конструктор класса не может быть вызван без new:
+
+    // - Методы класса являются неперечислимыми. ENUMARABLE : FALSE для всех методов в "prototype"
+
+    // - Классы всегда используют use strict
+
+}
+
+
+
+{ //Class Expression
+    {
+        let Aboba = class Bad{
+            say(){
+                console.log(Bad)
+            }
+        };
+
+        new Aboba().say() // выводит вот это вот все
+        //Bad name извне класса недоступен 
+    }
+
+    {// динамическое создрания по запросу
+        function makeClass(phrase) {
+            // объявляем класс и возвращаем его
+            return class {
+              sayHi() {
+                alert(phrase);
+              };
+            };
+          }
+          
+          // Создаём новый класс
+          let User = makeClass("Привет");
+          
+          new User().sayHi(); // Привет
+    }
+}
+
+
+
+{//get set etc
+    {
+        class Aboba{
+
+            constructor(age){
+                this.age = age;
+            }
+
+            get age() {
+                return this._age;
+            }
+
+            set age(value){
+                if (value < 20){
+                    console.log('ты мал');
+                    return;
+                }
+                this._age = value;
+            }
+        }
+
+        let aboba = new Aboba(27);
+        console.log(aboba.age); //27
+
+        zopa = new Aboba(18); // ты мал
+    }
+
+    { // можно юзать вычисляемые свойства
+        ['say' + 'Hi']() {
+            alert("");
+          }
+    }
+
+    {//При объявлении класса геттеры/сеттеры создаются на Aboba.prototype, вот так:
+        Object.defineProperties(Aboba.prototype, {
+            name: {
+              get() {
+                return this._name
+              },
+              set(name) {
+                // ...
+              }
+            }
+          });
+    }
+}
+
+
+
+{//Свойства классов 
+
+    class User {
+        name = "Аноним";
+      
+        sayHi() {
+          alert(`Привет, ${this.name}!`);
+        }
+      }
+      
+      new User().sayHi();
+
+    //   Свойство name не устанавливается в User.prototype. 
+    //   Вместо этого оно создаётся оператором new перед запуском конструктора, это именно свойство объекта.
+}

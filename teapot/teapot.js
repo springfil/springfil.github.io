6c38539ce231764d6679f78825045fc3447d3a25
springfil <st.kenzhaev@gmail.com>
2023-05-27 20:17:09 +0300

    PourOutWater(amount) + флаг нагрева

--- teapot/teapot.js
+++ teapot/teapot.js
@@ -1,79 +1,105 @@
-class Teapot{
-    constructor(power){
-      this.power = power  // мощность (ватт)
-      this.waterAmount = 0; // уровень воды (вес в мг)
-      this.waterHeatCapipacity = 4200; // удельная темплоемкость воды
-      this.maxWaterLevel = 1500; 
-      this.Connected = false; // чайник не в сети
-      this.timerId = null;
+class Teapot {
+  constructor(power) {
+    this.power = power; // мощность (ватт)
+    this.waterAmount = 0; // уровень воды (вес в мг)
+    this.waterHeatCapipacity = 4200; // удельная темплоемкость воды
+    this.maxWaterLevel = 1500;
+    this.Connected = false; // чайник не в сети
+    this.timerId = null;
+    this.heating = false;
+  }
+  //формула для расчета HeatingTime = c*m*ΔT / power
+  //с - удельная темплоемкость воды / m - уровень воды
+  // ΔT - насколько нагреть power - мощность
+  HeatingTime() {
+    let time = (this.waterAmount * this.waterHeatCapipacity * 74) / this.power;
+    return time;
+  }
+
+  CompleteHeating() {
+    console.log(`Закипел!`);
+    clearTimeout(this.timerId);
+    this.timerId = null;
+  }
+
+  CheckWaterLevel() {
+    if (this.waterAmount === 0) {
+      console.log(
+        "Ошибка! Чайник пустой. Добавить воды instance.waterAmount = somevalue"
+      );
+      return false;
     }
-    //формула для расчета HeatingTime = c*m*ΔT / power
-    //с - удельная темплоемкость воды / m - уровень воды 
-    // ΔT - насколько нагреть power - мощность 
-    HeatingTime() {
-      let time = this.waterAmount * this.waterHeatCapipacity * 74 / this.power;
-      return time
+
+    if (this.waterAmount > this.maxWaterLevel) {
+      console.log(
+        "Ошибка! Вы перелили воду. Отлить instance.PourOutWater(somevalue)"
+      );
+      this.waterAmount = this.maxWaterLevel;
+      return false;
     }
-  
-    CompleteHeating() {
-      console.log(`Закипел! Время закипания - ${this.HeatingTime()} мс`);
-      clearTimeout(this.timerId);
+
+    return true;
+  }
+
+  CheckConnection() {
+    if (!this.Connected) {
+      console.log(
+        "Ошибка! Не включен в розетку.Включить в сеть instance.Connected = true "
+      );
+      return false;
     }
-  
-    CheckWaterLevel() {
-      if (this.waterAmount === 0) {
-        console.log('Ошибка! Чайник пустой. Добавить воды instance.waterAmount = somevalue');
-        return false;
+
+    return true;
+  }
+
+  PourOutWater(amount) {
+    if (amount > this.waterAmount) {
+      console.log(
+        "Ошибка! Нельзя отлить больше, чем есть воды. Вы вылили всю воду!"
+      );
+      this.waterAmount = 0;
+      return false;
+    }
+
+    this.waterAmount -= amount;
+    console.log(
+      `Вы вылили ${amount} мл воды. Текущий уровень воды: ${this.waterAmount} мл.`
+    );
+
+    // if (this.waterAmount <= this.maxWaterLevel) {
+    //   this.start()
+    // }
+
+    return true;
+  }
+
+  start() {
+    if (this.CheckConnection() && this.CheckWaterLevel()) {
+      if (this.heating) {
+        console.log("Ошибка! Чайник уже включен. Дождитесь окончания работы");
+        return;
       }
-      
-      if (this.waterAmount > this.maxWaterLevel) {
-        console.log('Ошибка! Уровень воды превышает максимальный уровень.');
-        return false;
-      }
-      
-      return true;
-    }
-  
-    CheckConnection(){
-      if (!this.Connected){
-        console.log('Ошибка! Не включен в розетку.Включить в сеть instance.Connected = true ');
-        return false;
-      }
-      
-      return true;
-    }
-  
-    pourOutWater(amount) {
-      if (amount > this.waterAmount) {
-        console.log('Ошибка! Нельзя отлить больше, чем есть воды.');
-        return false;
-      }
-  
-      this.waterAmount -= amount;
-      console.log(`Вы вылили ${amount} мл воды. Текущий уровень воды: ${this.waterAmount} мл.`);
-  
-      return true;
-    }
-  
-    start() {
-      if (this.CheckConnection() && this.CheckWaterLevel()) {
-        this.timerId = setTimeout(() => this.CompleteHeating(), this.HeatingTime());
-      }
+
+      this.timerId = setTimeout(() => {
+        this.CompleteHeating();
+        this.heating = false; 
+      }, this.HeatingTime());
+      console.log(`Чайник включен. Время закипания - ${this.HeatingTime()} мс`);
+      this.heating = true;  
     }
   }
-  
-  
-//   const teapot = new Teapot(200000); 
-//   teapot.start(); 
-//   teapot.Connected = true;
-//   teapot.start();
-//   teapot.waterAmount = 2000;
-//   teapot.start();
-//   teapot.pourOutWater(500);
-//   teapot.start()
-  
-  
-  
-  
-  
-  
+}
+
+
+// const teapot = new Teapot(20000); // чайник большой мощности
+// teapot.start();
+// teapot.Connected = true;
+// teapot.start();
+// teapot.waterAmount = 2000; //добавили выше уровня
+// teapot.start();
+// teapot.PourOutWater(1600); // вылили больше чем есть
+// teapot.start();
+// teapot.waterAmount = 500; // налили нормально
+// teapot.start();
+// teapot.start();
+// teapot.start();
